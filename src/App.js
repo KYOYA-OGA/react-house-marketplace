6d11abcb649ffb878c0c7f9d308956fe3a464fb2
KYOYA OGASAWARA <kyoyaoga@gmail.com>
2021-12-22 16:31:13 +0700

    created profile, explore pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SignUp from './pages/SignUp';
+import PrivateRoute from './components/PrivateRoute';
+import Offers from './pages/Offers';
+import SignIn from './pages/SignIn';
+import Profile from './pages/Profile';
+import CreateListing from './pages/CreateListing';
+import ForgotPassword from './pages/ForgotPassword';
+import Explore from './pages/Explore';
+import Navbar from './components/Navbar';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Category from './pages/Category';
 
 function App() {
   return (
     <>
       <Router>
-        <Routes></Routes>
+        <Routes>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+        </Routes>
+        <Navbar />
       </Router>
+      <ToastContainer />
     </>
   );
 }
